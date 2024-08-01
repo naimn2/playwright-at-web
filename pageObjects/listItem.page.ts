789e1a4136092aafe66d617498f1ae8cf8eb3a70
Muhammad Muflihun Naim <muhammad.muflihun@admo.astra.co.id>
2024-08-01 22:18:10 +0700

    membuat locator card item di List item Page object

--- pageObjects/listItem.page.ts
+++ pageObjects/listItem.page.ts
@@ -1,3 +1,10 @@
+import { Locator, Page } from '@playwright/test';
 export class ListItemPage {
-    
+    page: Page;
+    cardItem: Locator;
+
+    constructor(page: Page) {
+        this.page = page;
+        this.cardItem = page.locator('');
+    }
 }
