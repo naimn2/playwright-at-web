aa531cd3e6e682785190b7ef085dd28f9f171618
Muhammad Muflihun Naim <muhammad.muflihun@admo.astra.co.id>
2024-08-01 20:03:36 +0700

    menambahkan locator motor category pada category page object

--- pageObjects/category.page.ts
+++ pageObjects/category.page.ts
@@ -2,9 +2,11 @@ import { Locator, Page } from '@playwright/test';
 export class CategoryPage {
     page: Page;
     btnCarCategory: Locator;
+    btnMotorCategory: Locator;
 
     constructor(page: Page) {
         this.page = page;
         this.btnCarCategory = page.locator('');
+        this.btnMotorCategory = page.locator('');
     }
 }
