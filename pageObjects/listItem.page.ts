4e40fd2a5ebcf2e3c897b1818177f59e04c6574f
Muhammad Muflihun Naim <muhammad.muflihun@admo.astra.co.id>
2024-08-01 22:20:30 +0700

    menambahkan locator btnCall di List Item Page

--- pageObjects/listItem.page.ts
+++ pageObjects/listItem.page.ts
@@ -2,9 +2,11 @@ import { Locator, Page } from '@playwright/test';
 export class ListItemPage {
     page: Page;
     cardItem: Locator;
+    btnCall: Locator;
 
     constructor(page: Page) {
         this.page = page;
-        this.cardItem = page.locator('');
+        this.cardItem = page.getByTestId('cardItem');
+        this.btnCall = page.getByTestId('btnCall');
     }
 }
