9313929529fac8a69ad89e73b22601382703454f
Muhammad Muflihun Naim <muhammad.muflihun@admo.astra.co.id>
2024-08-01 20:00:34 +0700

    menambahkan locator car category button on category page object

--- pageObjects/category.page.ts
+++ pageObjects/category.page.ts
@@ -1,3 +1,10 @@
+import { Locator, Page } from '@playwright/test';
 export class CategoryPage {
-    
+    page: Page;
+    btnCarCategory: Locator;
+
+    constructor(page: Page) {
+        this.page = page;
+        this.btnCarCategory = page.locator('');
+    }
 }
