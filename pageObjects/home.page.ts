7bf84dde662a2746b0724d9ef98ac5497cf5f43d
Muhammad Muflihun Naim <muhammad.muflihun@admo.astra.co.id>
2024-08-01 22:26:15 +0700

    menambahkan locator button my account di home page

--- pageObjects/home.page.ts
+++ pageObjects/home.page.ts
@@ -3,10 +3,12 @@ import { Locator, Page } from '@playwright/test';
 export class HomePage {
     page: Page;
     btnBurger: Locator;
+    btnMyAccount: Locator;
 
     constructor(page: Page) {
         this.page = page;
         this.btnBurger = page.locator('');
+        this.btnMyAccount = page.getByTestId('btnMyAccount');
     }
 
     async clickBtnBurger() {
